304c17d8a058092d113148b50cc89b79a96185b7
hyt-nnht <hayatozhongcun909@gmail.com>
2026-09-01 20:24:51 +0900

    JobObjectManager.csの作成

--- FileOrganizer.Infrastructure/obj/Debug/net10.0-windows10.0.19041.0/FileOrganizer.Infrastructure.AssemblyInfo.cs
+++ FileOrganizer.Infrastructure/obj/Debug/net10.0-windows10.0.19041.0/FileOrganizer.Infrastructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FileOrganizer.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+af3e611800e0f2f3fc2284dcb258900758f63b7d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0b395aea440610c706ef41f3bc12bb02cfd9b289")]
 [assembly: System.Reflection.AssemblyProductAttribute("FileOrganizer.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FileOrganizer.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
